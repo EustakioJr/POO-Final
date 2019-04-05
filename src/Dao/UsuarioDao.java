7377596111c7154dfa81accb4afb4057112b0459
Alex-PC\Alex <almeidafer92@gmail.com>
2019-04-05 03:10:59 -0300

    JavaDoc acrescentado

--- src/Dao/UsuarioDao.java
+++ src/Dao/UsuarioDao.java
@@ -9,9 +9,44 @@ import java.util.Set;
 public interface UsuarioDao {
 
     Set<Usuario> getUsuarios() throws IOException, ClassNotFoundException, SQLException;
+
+    /**
+     * <b>Função de salvar</b>
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     * @throws IOException
+     */
+
     boolean salvar(Usuario usuario) throws IOException, ClassNotFoundException, SQLException;
+
+    /**
+     * <b>Função de busca os Ususarios.</b>
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+
     public Usuario buscarPorUsername(String username) throws SQLException, ClassNotFoundException;
+
+    /**
+     * <b>Função de deletar Usuarios.</b>
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     * @throws IOException
+     */
+
     boolean deletar(Usuario usuario) throws IOException, ClassNotFoundException, SQLException;
+
+    /**
+     * <b>Função de atualizar Usuarios.</b>
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     * @throws IOException
+     */
+
     boolean atualizar(Usuario usuario) throws IOException, ClassNotFoundException, SQLException;
 
 }
