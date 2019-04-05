1a2ea9ae07dd11c51088654c8b2f39312177bfd4
Alex-PC\Alex <almeidafer92@gmail.com>
2019-04-05 08:31:09 -0300

    Tela consulta mudada

--- src/Controle/ControleTelaListaClinicaUser.java
+++ src/Controle/ControleTelaListaClinicaUser.java
@@ -2,9 +2,20 @@ package Controle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 
 public class ControleTelaListaClinicaUser {
+
+
+    @FXML
+    private Label ClinicaNome;
+
+    @FXML
+    private Label ClinicaCNPJ;
+
+    @FXML
+    private Label ClinicaEndereco;
 
     @FXML
     private MenuItem menuHome;
